797ff898c3175f361b42792b6d4f73d72c9bcdc8
Francisco Cezar <102926017+franciscocezar@users.noreply.github.com>
2022-04-21 20:00:58 -0300

    Conversor de moeda.

---
+++ ConversorDeMoeda.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ProjetoCotação
+{
+    public class ConversorDeMoeda
+    {
+        public static double Iof = 6.0;
+
+        public static double Conversor(double dolar, double cotacao)
+        {
+            double total = dolar * cotacao;
+            return total + total * Iof / 100.0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ProjetoCotação
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double cot = double.Parse(Console.ReadLine());
+            Console.Write("Quantos dólares você vai comprar? ");
+            double dolar = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Valor a ser pago em reais = "
+                + ConversorDeMoeda.Conversor(dolar, cot).ToString("F2"));
+
+
+        }
+    }
+}
